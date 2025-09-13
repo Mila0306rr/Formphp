914e8f47fded9afebef37e188330098997dbaea3
Mila0306rr <jemila0306@gmail.com>
2025-09-13 09:22:29 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -9,4 +9,5 @@ $cargo = $_POST['cargo'];
 echo "<h2>Información recibida</h2>";
 echo "El empleado $nombre tiene $edad años.<br>";
 echo "Vive en $direccion y su salario es: $salario.";
+echo "Su correo es $email y su cargo es $cargo.";
 ?>
